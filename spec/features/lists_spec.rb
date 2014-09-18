a632cd0feb7badae9880100f34b0d8f25314425d
Aubrey Howell <simplyaubs@gmail.com>
2014-09-17 22:06:45 -0400

    User can update a list

--- spec/features/lists_spec.rb
+++ spec/features/lists_spec.rb
@@ -12,4 +12,26 @@ feature 'Brunch Lists' do
     expect(page).to have_content 5
   end
 
+  scenario 'User can update a restaurant from list' do
+    visit '/'
+    expect(page).to have_content 'Welcome'
+    click_on 'Add a brunch spot'
+    fill_in 'Restaurant', with: 'Hopscotch'
+    fill_in 'Rating', with: 5
+    click_on 'Add brunch spot'
+    expect(page).to have_content 'Hopscotch'
+    expect(page).to have_content 5
+    click_on 'Hopscotch'
+    expect(page).to have_content 'Hopscotch'
+    expect(page).to have_content 5
+    click_on 'Edit'
+    fill_in 'Restaurant', with: 'Eats'
+    fill_in 'Rating', with: 4
+    click_on 'Update Restaurant'
+    expect(page).to_not have_content 'Hopscotch'
+    expect(page).to_not have_content 5
+    expect(page).to have_content 'Eats'
+    expect(page).to have_content 4
+  end
+
 end
